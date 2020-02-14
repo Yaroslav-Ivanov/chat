71f9a9b54a7cfa9118f6c2831620a0b1aa14b02e
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-02-14 18:44:23 +0300

    banIP

--- showmassege.php
+++ showmassege.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        .ban{
+        .ban {
             margin-left: 500px;
             color: red;
             font-size: 100px
         }
     </style>
 </head>
+
 <body>
     <form action="?" method="POST">
-    <input type="text"  name="name" value="<?=!empty($_POST['name'])?$_POST['name']:"" ?>" >
-    <input type="text" name="chat">  
-    <input type="submit" value="ok">
+        <input type="text" name="name" value="<?= !empty($_POST['name']) ? $_POST['name'] : "" ?>">
+        <input type="text" name="chat">
+        <input type="submit" value="ok">
     </form>
-    
-<?php
-     if (($_SERVER['REMOTE_ADDR'] != '127.0.0.1')&&(!empty($_POST["name"]))) {
-        file_put_contents("mess.txt",$_SERVER['HTTP_USER_AGENT']. ":". $_SERVER['REMOTE_ADDR']. ":". $_POST["name"].":". $_POST["chat"] ."\n", FILE_APPEND );
-    }else{
+
+    <?php
+    $ban = file("ban.txt");
+
+    if (in_array($_SERVER['REMOTE_ADDR'], $ban)) {
         echo  "<div class='ban'>Произошел БАН!<div>";
+    } else {
+        file_put_contents("mess.txt", $_SERVER['HTTP_USER_AGENT'] . ":" . $_SERVER['REMOTE_ADDR'] . ":" . $_POST["name"] . ":" . $_POST["chat"] . "\n", FILE_APPEND);
     }
 
-?>
+    ?>
 </body>
+
 </html>
